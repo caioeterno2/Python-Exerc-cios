ea93433ef46314a756a8256e9e01f3c16ad3f3aa
Erik cavalcante <trababundante@gmail.com>
2024-05-20 18:28:31 -0300

    Criei a lista de exercícios
    Adicionei exercício 01

---
+++ Lista de Exercicios/Exercicio01.py
@@ -0,0 +1,13 @@
+'''
+ Um algoritmo que armazene à variável "A" o valor "10" e para variável "B" o valor "20". A seguir troque os valores dessas variáveis entre si escrevendo o resultado na tela.
+'''
+
+a = int(input('A = '))
+b = int(input('B = '))
+troca = 0
+
+troca = a 
+a = b
+
+print('A = ',a)
+print('B = ',int(troca))
