28486d58d4704b92c2e816031f749088e4af12bd
Erik cavalcante <trababundante@gmail.com>
2024-05-22 14:59:37 -0300

    Troca dos valores

--- Lista de Exercicios/Exercicio01.py
+++ Lista de Exercicios/Exercicio01.py
@@ -1,10 +1,12 @@
 '''
  Um algoritmo que armazene à variável "A" o valor "10" e para variável "B" o valor "20". A seguir troque os valores dessas variáveis entre si escrevendo o resultado na tela.
 '''
+print('| TROCA DE VALORES |')
+print('====================')
 
 a = int(input('A = '))
 b = int(input('B = '))
-troca = 0
+print('_________')
 
 troca = a 
 a = b
