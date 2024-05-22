1f027e53426facbc2b240fb30206f6daebd628aa
Erik cavalcante <trababundante@gmail.com>
2024-05-22 14:43:25 -0300

    Área do Retângulo

---
+++ Lista de Exercicios/Exercicio04.py
@@ -0,0 +1,13 @@
+'''
+Escreva um algoritmo que calcule e escreva a área de retângulo. 
+'''
+print('| ÁREA DO RETÂNGULO |')
+print('=====================')
+
+base = float(input('Quanto mede a base do retângulo (metros): '))
+alto = float(input('Quanto mede a altura do retângulo (metros): '))
+print('_______________________________')
+
+area = base * alto
+
+print('A área desse retângulo é de',area,'m2')
