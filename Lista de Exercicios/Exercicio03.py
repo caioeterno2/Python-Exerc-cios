f462e47daf9ea2d381558c8009dc41572e7110e8
Erik cavalcante <trababundante@gmail.com>
2024-05-22 14:05:34 -0300

    Antecessor e Sucessor

---
+++ Lista de Exercicios/Exercicio03.py
@@ -0,0 +1,12 @@
+'''
+Escreva um algoritmo para ler um valor e mostrar seu antecessor e sucessor.
+'''
+
+val = int(input('Digite um valor inteiro: '))
+print('________________________')
+
+ante = val - 1
+suce = val + 1
+
+print('.Seu ANTECESSOR é',ante)
+print('.Seu SUCESSOR é',suce)
