94b1a003c149c2e934f998e9e812f718c5fd34da
Erik cavalcante <trababundante@gmail.com>
2024-05-22 14:53:37 -0300

    Pares iguais

--- Lista de Exercicios/Exercicio02.py
+++ Lista de Exercicios/Exercicio02.py
@@ -1,14 +1,16 @@
 '''
 Crie um algoritmo que mostre se os pares abaixo tem o mesmo resultado.
 '''
+print('| Igual ou Diferente |')
+print('======================')
 
 print('Os pares abaixo tem o mesmo resultado? [s/n] ')
 
-resp1 = str(input('A <- (4/2) + (2/4) e A <- 4/2 + 2/4: '))
+resp1 = str(input('A <- (4/2) + (2/4)  e  A <- 4/2 + 2/4: '))
 
-resp2 = str(input('B <- 4/(2+2)/4 e B <- 4/2 + 2/4: '))
+resp2 = str(input('B <- 4/(2+2)/4  e  B <- 4/2 + 2/4: '))
 
-resp3 = str(input('c <- (4+2) * 2-4 e C <- 4+2 * 2-4: '))
+resp3 = str(input('c <- (4+2) * 2-4  e  C <- 4+2 * 2-4: '))
 
 print('_________________________________________')
 print('Resposta:')
@@ -16,20 +18,20 @@ print('Resposta:')
 v = True
 f = False
 
-if resp1 == 's':
-    print('A <- (4/2) + (2/4) e A <- 4/2 + 2/4: ',v)
+if resp1 == 's' or 'S':
+    print('A <- (4/2) + (2/4)  e  A <- 4/2 + 2/4: ',v)
 else:
-    print('A <- (4/2) + (2/4) e A <- 4/2 + 2/4: ',f)
+    print('A <- (4/2) + (2/4)  e  A <- 4/2 + 2/4: ',f)
 
-if resp2 == 'n':
-    print('B <- 4/(2+2)/4 e B <- 4/2 + 2/4: ',v)
+if resp2 == 'n' or 'N':
+    print('B <- 4/(2+2)/4  e  B <- 4/2 + 2/4: ',v)
 else: 
-    print('B <- 4/(2+2)/4 e B <- 4/2 + 2/4: ',f)
+    print('B <- 4/(2+2)/4  e  B <- 4/2 + 2/4: ',f)
 
-if resp3 == 'n':
-    print('c <- (4+2) * 2-4 e C <- 4+2 * 2-4: ',v)
+if resp3 == 'n' or 'N':
+    print('c <- (4+2) * 2-4  e  C <- 4+2 * 2-4: ',v)
 else:
-    print('c <- (4+2) * 2-4 e C <- 4+2 * 2-4: ',f)
+    print('c <- (4+2) * 2-4  e  C <- 4+2 * 2-4: ',f)
 
 
 
