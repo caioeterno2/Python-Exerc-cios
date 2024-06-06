1fad00c0805dfb0f6a108c27597744a627f83658
Erik cavalcante <trababundante@gmail.com>
2024-06-06 17:44:51 -0300

    Salário T. do funcionário

---
+++ Lista de Exercicios/Exercicio08.py
@@ -0,0 +1,29 @@
+'''
+Escreva um algoritmo que leia o número de carros vendidos por um funcionário, o valor total de suas vendas, seu salário fixo, valor recebido por cada carro vendido e seu salário final. 
+'''
+print(' =============================')
+print(' | SALÁRIO T. DO FUNCIONÁRIO |')
+print(' =============================')
+
+cv = int(input(' Informe quantos carros foram vendidos pelo funcionário: '))
+print(' -----')
+sf = float(input(' Informe o salário fixo do funcionário: '))
+print(' -----')
+ccv = float(input(' Informe a comissão fixa recebida por cada carro vendido: '))
+print(' -----------------------------')
+
+vtv = cv * ccv 
+porc_5 = vtv * (5 / 100)
+vtv_f = vtv + porc_5
+sfi = sf + vtv + porc_5
+
+print(' Carros vendidos: {}'.format(cv))
+print(' Comissão por carro vendido: R${:.2f}'.format(ccv))
+print(' Valor total das vendas: R${:.2f}'.format(vtv_f))
+print(' Salário fixo: R${:.2f}'.format(sf))
+print(' Salário final: R${:.2f}'.format(sfi))
+print(' =============================')
+
+
+
+
