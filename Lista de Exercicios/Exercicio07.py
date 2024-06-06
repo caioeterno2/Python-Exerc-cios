b5bd5ee39dd5f6d3b142d9a26fbec13c83153903
Erik cavalcante <trababundante@gmail.com>
2024-06-06 11:24:09 -0300

    Custo do veículo

--- Lista de Exercicios/Exercicio07.py
+++ Lista de Exercicios/Exercicio07.py
@@ -1,9 +1,9 @@
 '''
 Escreva um algoritmo que leia o custo de fábrica de um carro junto com a % do distribuidor e dos impostos e escreva o custo final ao consumidor
 '''
-print(' ==================')
-print(' | CUSTO DO CARRO |')
-print(' ==================')
+print(' ====================')
+print(' | CUSTO DO VEÍCULO |')
+print(' ====================')
 
 cf = float(input(' Informe o custo de fabricação do veiculo: R$'))
 print(' -----')
@@ -17,4 +17,4 @@ porc_ip = cf * (ip / 100)
 val_final = cf + porc_pd + porc_ip
 
 print(' O veiculo será repassado ao consumidor com o valor total de R${:.3f}'.format(val_final))
-print(' ==================')
+print(' ====================')
