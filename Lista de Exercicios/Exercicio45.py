1323225840ca9fd8f9ec8abd6c982680619267d4
Erik cavalcante <trababundante@gmail.com>
2024-06-26 08:38:02 -0300

    Valores Inteiros

--- Lista de Exercicios/Exercicio45.py
+++ Lista de Exercicios/Exercicio45.py
@@ -7,10 +7,17 @@ print(' ====================')
 
 val = int(input(' Informe um número: '))
 print(' ---------')
+
+while val <= 0:
+    val = int(input(' Informe um número maior que 0: '))
+    print(' ---------')
+
 print(' Valores inteiros entre 1 e {}.'.format(val))
+print(' ---------')
 
 for cont in range(1,val + 1):
     print('',cont)
+
 
 print(' =====================')
     
