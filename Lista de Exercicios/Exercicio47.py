e13e2256ac05944b5b339fe82eddb71015c98e8c
Erik cavalcante <trababundante@gmail.com>
2024-07-01 21:30:31 -0300

    Contador

---
+++ Lista de Exercicios/Exercicio47.py
@@ -0,0 +1,16 @@
+'''
+Algoritmo de um contador (for)
+'''
+print(' ============')
+print(' | CONTADOR |')
+print(' ============')
+
+contador = 1
+
+for cont in range(1, 11):
+    print('',contador)
+    contador = contador + 1
+
+print(' ------')
+print(' Fim da contagem.')
+print(' ============')
