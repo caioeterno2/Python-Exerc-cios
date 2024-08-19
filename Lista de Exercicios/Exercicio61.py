7bcfb19dc8c6ce7c2300c5fe16930113277a78a6
Erik cavalcante <trababundante@gmail.com>
2024-08-19 10:23:41 -0300

    1 a 10

---
+++ Lista de Exercicios/Exercicio61.py
@@ -0,0 +1,10 @@
+print(' =====================')
+print(' | 1 a 10 - 10 VEZES |')
+print(' =====================')
+
+for cont in range(1, 11):
+    print(f' {cont}')
+    for ten in range(1, 11):
+        print(f' {ten}..')
+
+    print(' - - -')
