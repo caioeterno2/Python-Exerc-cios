a7f024c8a98757371a77866014dc92f8ce8aee12
Erik cavalcante <trababundante@gmail.com>
2024-09-01 10:53:50 -0300

    Média Ponderada

--- Lista de Exercicios/Exercicio10.py
+++ Lista de Exercicios/Exercicio10.py
@@ -1,18 +1,19 @@
 '''
-Escreva um algoritmo que receba 3 notas, calcule e escreva a média final, considerando que a média é ponderada e o peso das notas é 2, 3 e 5.
+ Leia 3 notas, calcule e escreva a média final, considerando que a média é ponderada e o peso das notas é 2, 3 e 5. - Ex. N° 13
 '''
+
 print(' ===================')
 print(' | MÉDIA PONDERADA |')
 print(' ===================')
  
-n1 = float(input(' Informe a 1* nota do aluno: '))
-print(' -----')
-n2 = float(input(' Informe a 2* nota do aluno: '))
-print(' -----')
-n3 = float(input(' Informe a 3* nota do aluno: '))
-print(' -------------------')
+n1 = float(input(' 1* nota do aluno: '))
+print(' - - - - - - - - -')
+n2 = float(input(' 2* nota do aluno: '))
+print(' - - - - - - - - -')
+n3 = float(input(' 3* nota do aluno: '))
+print(' = = = = = = = = =')
 
 media = ((n1 * 2) + (n2 * 3) + (n3 * 5)) / 10
 
-print(' A media final do aluno é de {} pontos.'.format(media))
+print(f' Média final: {media:.1f} pontos.')
 print(' ===================')
