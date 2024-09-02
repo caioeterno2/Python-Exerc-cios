9cf56899ba12188f99d633dcab60d3ed085db5f5
Erik cavalcante <trababundante@gmail.com>
2024-09-02 10:27:27 -0300

    Positive e Negativo

--- Lista de Exercicios/Exercicio12.py
+++ Lista de Exercicios/Exercicio12.py
@@ -1,16 +1,17 @@
 '''
-Escreva um algoritmo que leia e escreva se um valor é positivo ou negativo (considere 0 positivo).
+ Leia e escreva se um valor é positivo ou negativo (considere 0 positivo). - Ex. N° 15
 '''
-print(' ====================')
-print(' POSITIVO OU NEGATIVO')
-print(' ====================')
 
-val = float(input(' Digite um número: '))
-print(' --------------------')
+print(' ========================')
+print(' | POSITIVO OU NEGATIVO |')
+print(' ========================')
+
+val = int(input(' Digite um número (inteiro): '))
+print(' - - - - - - - - - - - - ')
 
 if val >= 0:
-    print(' O valor {:.0f} é um número POSITIVO'.format(val))
+    print(f' O {val} é um número POSITIVO.')
 else:
-    print(' O valor {:.0f} é um número NEGATIVO'.format(val))
+    print(f' O {val} é um número NEGATIVO.')
 
-print(' ====================')
+print(' ========================')
