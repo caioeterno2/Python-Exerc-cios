5e0cc6bc8a9ca01af6aa74522b5a457ceee64472
Erik cavalcante <trababundante@gmail.com>
2024-09-03 12:40:19 -0300

    Positivo ou Negativo

--- Lista de Exercicios/Exercicio24.py
+++ Lista de Exercicios/Exercicio24.py
@@ -1,18 +1,19 @@
 '''
-Escreva um algoritmo que leia um valor e escreva se é positivo, negativo ou zero
+ Leia um valor e escreva se é positivo, negativo ou zero. - Ex. N° 27
 '''
+
 print(' ========================')
 print(' | POSITIVO OU NEGATIVO |')
 print(' ========================')
 
 val = float(input(' Digite um número: '))
-print(' ------------------------')
+print(' - - - - - - - - - ')
 
 if val >= 1:
-    print(' Número Positivo')
+    print(' Número: Positivo')
 elif val == 0:
     print(' O número é ZERO')
 else:
-    print(' Número Negativo')
+    print(' Número: Negativo')
 
 print(' ========================')
