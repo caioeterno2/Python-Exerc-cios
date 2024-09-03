ab3c2eb249d83aac551432d83f87e410fb8844f1
Erik cavalcante <trababundante@gmail.com>
2024-09-03 12:35:09 -0300

    Peso Ideal

--- Lista de Exercicios/Exercicio20.py
+++ Lista de Exercicios/Exercicio20.py
@@ -1,21 +1,22 @@
 '''
-Escreva um algoritmo que mostre o peso ideal masculino e feminino
-'''
+ Mostre o peso ideal masculino e feminino. - Ex. N° 23
+''' 
+
 print(' ================')
 print(' |  PESO IDEAL  |')
 print(' ================')
 
-nome = str(input(' Informe o nome da pessoa: '))
-print(' ----')
-alt = float(input(' Informe a altura: '))
-print(' ----')
+nome = str(input(' Nome da pessoa: '))
+print(' - - - - - - - -')
+altu = int(input(' Altura (ex: 185): cm '))
+print(' - - - - - - - -')
 sexo = str(input(' Informe o sexo [m/f]: '))
-print(' ----------------')
+print(' = = = = = = = =')
 
-if sexo == 'm':
-    p_ideal = (72.7 * alt) - 58
+if sexo == 'm' or 'M':
+    p_ideal = 52 + (0.75 * (altu - 152.4)) 
 else:
-    p_ideal = (62.1 * alt) - 44.7
+    p_ideal = 52 + (0.67 * (altu - 152.4))
 
-print(' {}, com base na sua altura e seu sexo seu peso ideal é de {:.2f}kg.'.format(nome,p_ideal))
+print(f' Sobre {nome}, com base a altura e sexo seu peso ideal é de {p_ideal}kg.')
 print(' ================')
