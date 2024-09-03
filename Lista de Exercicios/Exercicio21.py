6ab3bcdfdddaa5bddea84cdd0133c98ec3a841f4
Erik cavalcante <trababundante@gmail.com>
2024-09-03 12:35:45 -0300

    Salário do Funcionário

--- Lista de Exercicios/Exercicio21.py
+++ Lista de Exercicios/Exercicio21.py
@@ -1,23 +1,26 @@
 '''
-Escreva um algoritmo que leia o salário fixo e a quantidade de vendas de um funcionario calcule sua comissão e escreva seu salário fixo
+ Leia o salário fixo, quantidade de vendas do funcionario, calcule sua comissão e escreva o novo salário. - Ex. N° 24
 '''
-print(' ============================')
-print(' | SALÁRIO DO FUNCIONÁRIO 3 |')
-print(' ============================')
 
-nome = str(input(' Informe o nome do funcionário: '))
-print(' ----')
-sala = float(input(' Informe o salário fixo do funcionário: R$'))
-print(' ----')
-venda_t = float(input(' Informe o valor total das vendas deste funcionário: R$'))
-print(' ----------------------------')
+print(' ==========================')
+print(' | SALÁRIO DO FUNCIONÁRIO |')
+print(' ==========================')
 
-if venda_t <= 1500:
-    comis = venda_t * (3 / 100)
-else:
-    comis = venda_t * (5 / 100)
+nome = str(input(' Nome do funcionário: '))
+print(' - - - - - - - - - - - - - ')
+salario = float(input(' Salário do funcionário: R$'))
+print(' - - - - - - - - - - - - - ')
+venda_tot = float(input(' Valor total das vendas do funcionário: R$'))
+print(' = = = = = = = = = = = = = ')
 
-sala_t = sala + comis
+if (venda_tot <= 1500):
+    comiss_3 = venda_tot * (3 / 100)
+    salario_nov = salario + comiss_3
+elif (venda_tot > 1500):
+    comiss_3 = 1500 * (3 / 100)
+    a_mais = venda_tot - 1500
+    comiss_5 = a_mais * (5 / 100)
+    salario_nov = salario + comiss_3 + comiss_5
 
-print(' {}, seu salário total deste mês foi de R${:.2f} reais.'.format(nome,sala_t))
-print(' ============================')
+print(f' Salário total deste mês de {nome} foi R${salario_nov:.2f} reais.')
+print(' ==========================')
