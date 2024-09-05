581967155d50edd8eb881241e2b3b72938136677
Erik cavalcante <trababundante@gmail.com>
2024-09-05 18:42:51 -0300

    Desconto de Gasolina

--- Lista de Exercicios/Exercicio32.py
+++ Lista de Exercicios/Exercicio32.py
@@ -1,38 +1,39 @@
 '''
-Escreva um algoritmo que mostre o valor da compra de combustivel com desconto
+ Mostre o valor da compra de combustivel com desconto. - Ex. N° 35
 '''
-print(' =====================')
-print(' | POSTO DE GASOLINA |')
-print(' =====================')
+
+print(' ========================')
+print(' | DESCONTO DA GASOLINA |')
+print(' ========================')
 
 print(' A - Álcool  /  G - Gasolina')
-print(' --------')
+print(' - - - - - - - - - - - - ')
 
-tip = str(input(' Tipo de combustível: '))
+tipo = str(input(' Tipo de combustível: [A/G] '))
+print(' - - - - - - - - - - - - ')
 lt = int(input(' Litros: '))
-print(' --------')
+print(' = = = = = = = = = = = = ')
 
-if tip == 'A':
+if tipo == 'A' or 'a':
     tipo = 'Álcool'
     val = 2.90
     val_t = val * lt
     if lt <= 20:
-        porcen = val_t * (3 / 100)
-        val_f = val_t - porcen
+        porc_3 = val_t * (3 / 100)
+        val_f = val_t - porc_3
     else:
-        porcen = val_t * (5 / 100)
-        val_f = val_t - porcen       
-elif tip == 'G':
+        porc_5 = val_t * (5 / 100)
+        val_f = val_t - porc_5       
+elif tipo == 'G' or 'g':
     tipo = 'Gasolina'
     val = 3.30
     val_t = val * lt
     if lt <= 20:
-        porcen = val_t * (4 / 100)
-        val_f = val_t - porcen
+        porc_4 = val_t * (4 / 100)
+        val_f = val_t - porc_4
     else:
-        porcen = val_t * (6 / 100)
-        val_f = val_t - porcen
+        porc_6 = val_t * (6 / 100)
+        val_f = val_t - porc_6
 
-print(' Total de {:.0f} litros de {} foi de R${:.2f} reais.'.format(lt,tipo,val_f))
-print(' Volte sempre!')
-print(' =====================')
+print(f' {lt:.0f} litros de {tipo} vão dar R${val_f:.2f} reais.')
+print(' ========================')
