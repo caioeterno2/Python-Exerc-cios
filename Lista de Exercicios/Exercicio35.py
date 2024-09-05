df4d08cd37f096cf95cc727690e46a2a4a94d0f0
Erik cavalcante <trababundante@gmail.com>
2024-09-05 18:43:45 -0300

    Sistema de Segurança

--- Lista de Exercicios/Exercicio35.py
+++ Lista de Exercicios/Exercicio35.py
@@ -1,15 +1,13 @@
 '''
-Algoritmo de um código de segurança 
+ Escreva o algoritmo de um código de segurança. - Ex. N° 38
 '''
+
 print(' ========================')
 print(' | SISTEMA DE SEGURANÇA |')
 print(' ========================')
 
-print(' Código do Usuário: ')
-print(' ------')
-
 cod = int(input(' N* de código: '))
-print(' ------')
+print(' - - - - - - -')
 
 cod_interno = 1469
 senha_interna = 9999
@@ -18,7 +16,7 @@ if cod != cod_interno:
     print(" 'USUÁRIO INVÁLIDO!'")
 elif cod == cod_interno:
     senha = int(input(' Senha: '))
-    print(' -----------')
+    print(' - - - - - - -')
     if senha != senha_interna:
         print(" 'SENHA INCORRETA!'")
     elif senha == senha_interna:
