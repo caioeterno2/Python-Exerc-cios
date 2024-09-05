39e662a51795b8c5563df20b25668953d3bb4a8c
Erik cavalcante <trababundante@gmail.com>
2024-09-05 18:43:27 -0300

    Fruteira Desconto

--- Lista de Exercicios/Exercicio34.py
+++ Lista de Exercicios/Exercicio34.py
@@ -1,40 +1,42 @@
 '''
-Um algoritmo que aplica os descontos de uma fruteira
+ Aplique os descontos de uma fruteira. - Ex. N° 37
 '''
+
 print(' =====================')
 print(' | FRUTEIRA DESCONTO |')
 print(' =====================')
 
 print(' Informe a quantidade em (KG): ')
-print(' -------')
+print(' = = = = = = = = = = =')
 
-morango = float(input(' Morangos: KG -> '))
-maça = float(input(' Maçãs: KG -> '))
-print(' ---------------------')
+morangos = float(input(' Morangos: KG -> '))
+print(' - - - - -')
+maças = float(input(' Maçãs: KG -> '))
+print(' = = = = = = = = = = =')
 
-if morango <= 5:
+if morangos <= 5:
     valor_mo = 2.50
-    valor_mo_tot = valor_mo * morango
+    valor_totMO = valor_mo * morangos
 else:
     valor_mo = 2.20
-    valor_mo_tot = valor_mo * morango
+    valor_totMO = valor_mo * morangos
 
-if maça <= 5:
+if maças <= 5:
     valor_ma = 1.80
-    valor_ma_tot = valor_ma * maça
+    valor_totMA = valor_ma * maças
 else:
     valor_ma = 1.50
-    valor_ma_tot = valor_ma * maça
+    valor_totMA = valor_ma * maças
 
-kg_total = morango + maça
-valor_soma = valor_mo_tot + valor_ma_tot
-descon = valor_soma * (10 / 100)
+kg_total = morangos + maças
+soma = valor_totMO + valor_totMA
+descon = soma * (10 / 100)
 
-if kg_total >= 9 or valor_soma >= 26:
-    valor_total = valor_soma - descon
+if kg_total >= 9 or soma >= 26:
+    valor_total = soma - descon
 else:
-    valor_total = valor_soma
+    valor_total = soma
 
-print(' A quantidade comprada de morangos foi {:.0f}Kg e maçãs foi {:.0f}Kg.'.format(morango,maça))
-print(' A quantidade total foi {:.0f}Kg e o total a pagar é R${:.2f} reais.'.format(kg_total,valor_total))
+print(f' A quantidade de morangos foi {morangos:.0f}Kg e maçãs foi {maças:.0f}Kg.')
+print(f' A quantidade total foi {kg_total:.0f}Kg e o total a pagar é R${valor_total:.2f} reais.')
 print(' =====================')
