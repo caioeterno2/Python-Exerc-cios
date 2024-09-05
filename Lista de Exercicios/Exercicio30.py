5aceeef7ef53b4fbaff9571d975fc22971140df3
Erik cavalcante <trababundante@gmail.com>
2024-09-05 18:42:10 -0300

    Maior ou Igual

--- Lista de Exercicios/Exercicio30.py
+++ Lista de Exercicios/Exercicio30.py
@@ -1,22 +1,24 @@
 '''
-Escreva um algoritmo que leia 2 valores e escreva qual é maior ou se é iguais
+ Leia 2 valores e escreva qual é maior ou se são iguais. - Ex. N° 33
 '''
-print(' ========================')
-print(' | SITUAÇÃO DOS NÚMEROS |')
-print(' ========================')
+
+print(' ==================')
+print(' | MAIOR OU IGUAL |')
+print(' ==================')
 
 print(' Digite 2 números: ')
-print(' --------')
+print(' - - - - - - - - - ')
 
 num1 = int(input(' 1* valor: '))
+print(' - - - - -')
 num2 = int(input(' 2* valor: '))
-print(' ------------------------')
+print(' = = = = = = = = = ')
 
 if num1 > num2:
-    print(' Primeiro é maior.')
+    print(' Primeiro valor é maior.')
 elif num2 > num1:
-    print(' Segundo é maior.')
+    print(' Segundo valor é maior.')
 else:
     print(' Números iguais.')
 
-print(' ========================')
+print(' ==================')
